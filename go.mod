8e4c6517c1557ce8a85db903899e1ca5ff41ccf3
Aaron Jonk <AJ@jonk.name>
2025-03-04 10:59:39 +0100

    feat: :sparkles: Serve static files from public folder

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module codeberg.org/filesender/filesender-next
 go 1.23.4
 
 require github.com/mattn/go-sqlite3 v1.14.24
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- src/main.go
+++ src/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
-	"codeberg.org/filesender/filesender-next/config"
+	"codeberg.org/filesender/filesender-next/src/config"
 
+	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	// Load .env file
 	err := config.LoadEnv()
 	if err != nil {
 		log.Fatalf("Loading env file failed: %v", err)
@@ -22,12 +25,20 @@ func main() {
 		return
 	}
 
+	// Initialise database
 	db, err := config.InitDB(db_path)
 	if err != nil {
 		log.Fatalf("Failed initialising database: %v", err)
 		return
 	}
+	defer db.Close()
 
-	// Placeholder
-	_ = db
+	router := mux.NewRouter()
+
+	// Serve static files
+	fs := http.FileServer(http.Dir("./public"))
+	router.PathPrefix("/").Handler(fs)
+
+	port := "8080"
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
