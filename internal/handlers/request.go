2ed0b190b997e8811c8725ae2dd2005780c4b46b
Aaron Jonk <AJ@jonk.name>
2025-03-13 12:19:16 +0100

    chore: :pencil2: function -> body

--- internal/handlers/request.go
+++ internal/handlers/request.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-// Receive JSON function, tries to unmarshal the request body
+// Receive JSON body, tries to unmarshal the request body
 // If fails, returns a HTTP 400
 func recvJSON(w http.ResponseWriter, r *http.Request, v any) bool {
 	defer r.Body.Close()
