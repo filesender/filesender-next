fe4bb07236e70a8b9c61c774ec6402ca8975aeea
Aaron Jonk <AJ@jonk.name>
2025-04-16 15:10:59 +0200

    feat: :sparkles: Download read data to JS

--- internal/assets/public/js/download.js
+++ internal/assets/public/js/download.js
@@ -1,4 +1,6 @@
 
+const form = document.querySelector("form");
+
 /**
  * Decodes base64 string to bytes
  * @param {string} base64url 
@@ -14,3 +16,20 @@ const fromBase64Url = (base64url) => {
     }
     return bytes;
 }
+
+console.log(form);
+
+form.addEventListener("submit", async e => {
+    e.preventDefault();
+    await window.sodium.ready;
+    const sodium = window.sodium;
+
+    const formData = new FormData(form);
+    const userId = formData.get("user-id");
+    const fileId = formData.get("file-id");
+
+    const [key, header] = window.location.hash.substring(1).split(".").map(v => fromBase64Url(v));
+
+    console.log(userId, fileId);
+    console.log(key, header);
+});
