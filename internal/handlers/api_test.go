e2dc1af3c31df3f3371bb046931ae58c9ed417ff
Aaron Jonk <AJ@jonk.name>
2025-05-20 16:38:06 +0200

    fix: :white_check_mark: Initialise hashing package before testing

--- internal/handlers/api_test.go
+++ internal/handlers/api_test.go
@@ -27,6 +27,7 @@ func TestUploadAPIHandler(t *testing.T) {
 	}()
 
 	handler := handlers.UploadAPI("/", &auth.DummyAuth{}, tempDir, 10*1024*1024) // 10 MB limit
+	hash.Init(tempDir)
 
 	// Hash "dev" for test use
 	hashedID, err := hash.ToBase64("dev")
